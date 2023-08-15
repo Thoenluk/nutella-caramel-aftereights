c6c7d08a1f8aeef177188b270822eda2e3cd2f0a
thoenluk <Lukas.thoeni@gmx.ch>
2023-08-15 14:49:51 +0200

    Insert link to the Cataclysm Almanac (and add dropdown auto-open that Oto moaned about.)

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -29,7 +29,16 @@
 </script>
 
 <nav class="navbar navbar-expand-lg navbar-dark px-3 rounded-2">
-    <button type="button" class="btn border border-light rounded-2" on:click={() => navigationHierarchy.reset()}>Northrend</button>
+    <div class="btn-group border border-light rounded-2">
+        <button type="button" class="btn rounded-2" on:click={() => navigationHierarchy.reset()}>Northrend</button>
+        <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+            <span class="visually-hidden">Toggle Dropdown</span>
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item rounded-2" on:click={() => navigationHierarchy.reset()}>Northrend</a></li>
+            <li><a class="dropdown-item rounded-2" href="https://cataclysm-almanac.vercel.app/">The Cataclysm</a></li>
+        </ul>
+    </div>
     {#if $navigationHierarchy["category"]}
         <div class="btn-group border border-light ms-2 rounded-2">
             <button type="button" class="btn" on:click={() => setNavigationToCategory($navigationHierarchy["category"])}>{$navigationHierarchy["category"].name}</button>
@@ -80,5 +89,8 @@
     }
     .dropdown-item:hover {
         background: var(--button-red-hover) var(--bs-gradient);
+    }
+    .btn-group:hover .dropdown-menu {
+        display: block;
     }
 </style>
